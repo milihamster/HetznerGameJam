f211562637852531b3ce3b7f72d8edb8098d7bd3
MINUKKI-LAPTOP\dress <dressler.christoph@web.de>
2023-10-27 20:54:22 +0200

    Implemented EffectManager

---
+++ Assets/Scripts/EffectsManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectsManager : MonoBehaviour
+{
+    public static EffectsManager Instance { get; set; }
+
+    public EffectsManager()
+    {
+        Instance = this;
+    }
+
+    public void SpawnEffect(GameObject effect, GameObject origin)
+    {
+        Instantiate(effect, origin.transform);
+    }
+
+    public void SpawnEffect(GameObject effect, GameObject origin, Vector3 offset)
+    {
+        Vector3 newPosition = origin.transform.position + offset;
+        Instantiate(effect, newPosition, origin.transform.rotation);
+    }
+
+    public void SpawnEffect(GameObject effect, Vector3 position)
+    {
+        Instantiate(effect, position, effect.transform.rotation);
+    }
+}
