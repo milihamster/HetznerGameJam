89dda0225e8e2d0d6b2855cf838e773df0d430a7
MINUKKI-LAPTOP\dress <dressler.christoph@web.de>
2023-10-27 22:22:41 +0200

    Spawnmanager Prototype

---
+++ Assets/Scripts/EnemyType#.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyType
+{
+    SeaCreature1,
+    SeaCreature2,
+    LandCreature1,
+    LandCreature2,
+    SkyCreature1,
+    SkyCreature2
+}
