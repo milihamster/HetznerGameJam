98f1d44e494d5fd13c07c55a85a7c6d4501ca0b2
hopem <hopemilix@gmail.com>
2023-10-28 15:03:11 +0200

    Updated CameraController to include smooth animation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,23 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO: Fix weird bliue flash
 public class CameraController : MonoBehaviour
 {
     public static CameraController Instance;
 
     public Transform Target;
 
+    private Camera _camera;
+
     public CameraController()
     {
         Instance = this;
     }
 
+    void Start()
+    {
+        _camera = GetComponentInChildren<Camera>();
+    }
+
     void Update()
     {
-        transform.position = new(
-            Target.position.x,
-            Target.position.y,
-            -10 // 0 means the camera won't show anything
-            );
+        // If Target is far away:
+        // Move camera there smoothly
+        if(!LeanTween.isTweening(gameObject))
+        {
+            if (Vector2.Distance(
+                new(transform.position.x, transform.position.y),
+                new(Target.position.x, Target.position.y)) > 10)
+            {
+                LeanTween.move(gameObject, Target.position, 1f)
+                    .setEaseOutSine();
+            }
+            else
+                transform.position = new(
+                    Target.position.x,
+                    Target.position.y,
+                    -10 // 0 means the camera won't show anything
+                    );
+        }
     }
 }
