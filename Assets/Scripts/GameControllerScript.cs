1ca93aaaaf8fc279108921366f66a19c4706abdd
Bastian Reuther <bastian.reuther@allod.com>
2023-10-28 11:20:57 +0200

    Annimation + Fonts

--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -8,6 +8,10 @@ public class GameControllerScript : MonoBehaviour
     public GameObject pauseScreen;
     public GameObject inGameMenu;
 
+    public float popDuration = 1;
+    public Vector3 startScale = Vector3.zero;
+    public Vector3 endScale = Vector3.one;
+
     public bool GameIsRunning = true;
     void Update()
     {
@@ -32,12 +36,26 @@ public class GameControllerScript : MonoBehaviour
     public void Pause(){
         pauseScreen.SetActive(true);
         inGameMenu.SetActive(false);
+
+        inGameMenu.transform.localScale = startScale;
+        pauseScreen.transform.localScale = endScale;
+
+        LeanTween.scale(pauseScreen, startScale, 0.1f).setDelay(0.1f);
+        LeanTween.scale(inGameMenu, endScale, 0.1f).setDelay(0.1f);
+
         Time.timeScale = 0f;
         GameIsRunning = false;
     }
     public void Resume(){
         pauseScreen.SetActive(false);
         inGameMenu.SetActive(true);
+
+        inGameMenu.transform.localScale = startScale;
+        pauseScreen.transform.localScale = endScale;
+
+        LeanTween.scale(pauseScreen, startScale, 0.1f).setDelay(0.1f);
+        LeanTween.scale(inGameMenu, endScale, 0.1f).setDelay(0.1f);
+
         Time.timeScale = 1f;
         GameIsRunning = true;
     
