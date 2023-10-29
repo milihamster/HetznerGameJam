0dcefad67ed499257465cbe932e71b7e95e58d3c
hopem <hopemilix@gmail.com>
2023-10-29 12:51:44 +0100

    Removed test Pelican from scene, updated SpawnManager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -39,7 +39,7 @@ public class SpawnManager : MonoBehaviour
 
     void Start()
     {
-        //RespawnPlayer();
+        RespawnPlayer();
         //InvokeRepeating("SpawnPlanctons", 0, PlanctonSpawnRate);
 
         SpawnInitial(SpawnablesSea, SpawnAreaSea);
