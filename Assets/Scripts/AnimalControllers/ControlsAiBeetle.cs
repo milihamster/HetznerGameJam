04f0a1a09061103cc6dbd02cb5314de0c561357d
Robert Renkenberger <robert.renkenberger@hetzner.com>
2023-10-29 11:40:40 +0100

    Add Beetle

---
+++ Assets/Scripts/AnimalControllers/ControlsAiBeetle.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AnimalBeetle))]
+public class ControlsAiBeetle : ControlsAi
+{
+    public Vector3 _targetPosition;
+
+    [SerializeField]
+    private float _startCooldown = 10;
+    [SerializeField]
+    private float _range = 3;
+    [SerializeField]
+    private float _speed = 0.5f;
+    private float _positionCooldown;
+
+    private void LateUpdate()
+    {
+        Debug.DrawLine(transform.position, _targetPosition);
+    }
+
+    protected override void HandleControls()
+    {
+        if (_positionCooldown <= 0)
+        {
+            float x = (Random.Range(0, 2) * 2 - 1) * Random.Range(0.1f * _range, _range);
+            _targetPosition = transform.position + Vector3.right * x;
+
+            _positionCooldown = Random.Range(0.1f * _startCooldown, _startCooldown);
+        }
+        else
+            _positionCooldown -= Time.deltaTime;
+
+        MovementHorizontal = _targetPosition.x - transform.position.x;
+
+        if (Mathf.Abs(MovementHorizontal) < 0.1f)
+        {
+            MovementHorizontal = 0;
+        }
+
+        if (Mathf.Abs(MovementHorizontal) > _speed)
+        {
+            MovementHorizontal = Mathf.Sign(MovementHorizontal) * _speed;
+        }
+    }
+
+}
